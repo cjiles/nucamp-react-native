7c79811e2b815744bfa74edb0323a7f007efb992
cjiles <57699863+cjiles@users.noreply.github.com>
2020-02-22 12:06:44 -0600

    Week 3 Workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,6 +26,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true: false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true: false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -35,25 +36,29 @@ function RenderCampsite(props) {
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
-            if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites ?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    {cancelable: false}
-                );
-            }
+                if (recognizeDrag(gestureState)) {
+                    Alert.alert(
+                        'Add Favorite',
+                        'Are you sure you wish to add ' + campsite.name + ' to favorites ?',
+                        [
+                            {
+                                text: 'Cancel',
+                                style: 'cancel',
+                                onPress: () => console.log('Cancel Pressed')
+                            },
+                            {
+                                text: 'OK',
+                                onPress: () => props.favorite ?
+                                    console.log('Already set as a favorite') : props.markFavorite()
+                            }
+                        ],
+                        {cancelable: false}
+                    );
+
+                }
+                else if(recognizeComment(gestureState)) {
+                    props.onShowModal();
+                }
             return true;
         }
     });
@@ -143,8 +148,8 @@ class CampsiteInfo extends Component {
     this.setState({showModal: !this.state.showModal});
   }
 
-  handleComment(campsiteId){
-      this.props.postComment(campsiteId, this.state.rating, this.state.author, this.state.text);
+  handleComment(campsiteId, rating, author, text){
+      this.props.postComment(campsiteId, rating, author, text);
       this.toggleModal();
   }
 
--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -6,7 +6,7 @@ import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import Swipeout from 'react-native-swipeout';
 import { deleteFavorite } from '../redux/ActionCreators';
-import { Animatable } from 'react-native-animatable';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -55,14 +55,14 @@ class Favorites extends Component {
 
             return (
                 <Swipeout right={rightButton} autoClose={true}>
-                    {/* <Animatable.View animation='fadeInRightBig' duration={2000}> */}
+                    <Animatable.View animation='fadeInRightBig' duration={2000}>
                         <ListItem 
                             title={item.name}
                             subtitle={item.desription}
                             leftAvatar={{source: {uri: baseUrl + item.image}}}
                             onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
                         />
-                    {/* </Animatable.View> */}
+                    </Animatable.View>
                  </Swipeout>
             );
         };
